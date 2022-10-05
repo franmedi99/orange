cd63fee4cb645dbec490969303b967eedaac071c
franmedi99 <franmedi_mdp@hotmail.com>
2022-10-05 16:34:32 -0300

    a

---
+++ build/precache-manifest.80e56b60481bee65aac5371a4421166b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d883841f24a24416469",
+    "url": "/static/css/main.fccc10fd.chunk.css"
+  },
+  {
+    "revision": "5d883841f24a24416469",
+    "url": "/static/js/main.88e6e130.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ad8d2bb0d7211bde855",
+    "url": "/static/js/2.b638323f.chunk.js"
+  },
+  {
+    "revision": "ed3ce770568a6588dcea40fd12cb2c86",
+    "url": "/static/media/banco.ed3ce770.png"
+  },
+  {
+    "revision": "31d6c2ec52cd1ec56da2ba7d3bd8f83f",
+    "url": "/static/media/logo2.31d6c2ec.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "a34ed3bb0156431e07d860fc609899da",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.set('port', process.env.PORT || 4000);
 app.use(express.static(path.join(__dirname, 'build')));
 
 
- app.get('*', function (req, res) {
-     res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+//  app.get('*', function (req, res) {
+//      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
  
 
 //starting
